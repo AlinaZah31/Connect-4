81e24e88c711b54089a69ba122d5d841aa42345d
AlinaZah31 <alina_shuvi@yahoo.com>
2024-08-08 17:02:29 +0200

    Update script.js (add magic numbers and function initializeBoard)

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 currentPlayer = "red";
 let weHaveWinner = 0; 
 let board = [];
-for (let i = 0; i < 6; ++i) {
-    board[i] = [];
-    for (let j = 0; j < 7; ++j) {
-        board[i][j] = (i + 1) * 10 + j + 1;
+let noLines = 6, noCol = 7, TEN = 10;
+
+initializeBoard();
+
+function initializeBoard() {  
+    for (let i = 0; i < noLines; ++i) {
+        board[i] = [];
+        for (let j = 0; j < noCol; ++j) {
+            board[i][j] = (i + 1) * TEN + j + 1;
+        }
     }
 }
 
 generateButtons();
 
 function generateButtons() {
-    for (let i = 1; i <= 6; ++i) {
-        for (let j = 1; j <= 7; ++j) {
+    for (let i = 1; i <= noLines; ++i) {
+        for (let j = 1; j <= noCol; ++j) {
             const button = document.createElement("button");
             button.type = "button";
-            button.id = i * 10 + j;
+            button.id = i * TEN + j;
             button.innerText = button.id;
             document.body.appendChild(button);    
             button.onclick = "this.disabled=true";
             button.onclick = function() {modifyCell(button.id, i, j); this.disabled=true;}
-            if (j == 7 ) {
+            if (j == noCol ) {
                 document.body.appendChild(document.createElement("br"));
             }
         }  
@@ -44,12 +50,12 @@ function modifyCell(idBtn, lineIndex, colIndex) {
 
 function checkWinner(weHaveWinner1) {
     weHaveWinner1 = 0;  
-    for (let i = 0; i < 6 && weHaveWinner1 == 0; ++i) {
-        for (let j = 0; j < 7 && weHaveWinner1 == 0; ++j) {
-            if ((j < 4 &&  board[i][j] == board[i][j + 1] && board[i][j + 1] == board[i][j + 2] && board[i][j + 2] == board[i][j + 3]) || 
-                (i < 4 && board[i][j] == board[i + 1][j] && board[i + 1][j] == board[i + 2][j] && board[i + 2][j] == board[i + 3][j]) ||
-                (i < 4 && j < 4 && board[i][j] == board[i + 1][j + 1] && board[i + 1][j + 1] == board[i + 2][j + 2] && board[i + 2][j + 2] == board[i + 3][j + 3]) ||
-                 i < 4 && j > 3 && (board[i][j] == board[i + 1][j - 1] && board[i + 1][j - 1] == board[i + 2][j - 2] && board[i + 2][j - 2] == board[i + 3][j - 3])) {
+    for (let i = 0; i < noLines && weHaveWinner1 == 0; ++i) {
+        for (let j = 0; j < noCol && weHaveWinner1 == 0; ++j) {
+            if ((j <= noCol / 2 &&  board[i][j] == board[i][j + 1] && board[i][j + 1] == board[i][j + 2] && board[i][j + 2] == board[i][j + 3]) || 
+                (i <= noCol / 2 && board[i][j] == board[i + 1][j] && board[i + 1][j] == board[i + 2][j] && board[i + 2][j] == board[i + 3][j]) ||
+                (i <= noCol / 2 && j <= noCol / 2 && board[i][j] == board[i + 1][j + 1] && board[i + 1][j + 1] == board[i + 2][j + 2] && board[i + 2][j + 2] == board[i + 3][j + 3]) ||
+                 i <= noCol / 2 && j > noCol / 2 && (board[i][j] == board[i + 1][j - 1] && board[i + 1][j - 1] == board[i + 2][j - 2] && board[i + 2][j - 2] == board[i + 3][j - 3])) {
                 document.getElementById("Initial-text").innerHTML = "Winner is color: " + currentPlayer;
                 weHaveWinner1 = 1; 
                 weHaveWinner = weHaveWinner1;
