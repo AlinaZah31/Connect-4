40953ca8299929cc795c685a214563e8cc5140cb
AlinaZah31 <alina_shuvi@yahoo.com>
2024-08-08 09:45:58 +0200

    Update script.js (matrix initialization modified)

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 currentPlayer = "red";
 let weHaveWinner = 0; 
-let mat = [
-    [11, 12, 13, 14, 15, 16, 17],
-    [21, 22, 23, 24, 25, 26, 27],
-    [31, 32, 33, 34, 35, 36, 37],
-    [41, 42, 43, 44, 45, 46, 47],
-    [51, 52, 53, 54, 55, 56, 57],
-    [61, 62, 63, 64, 65, 66, 67],
-] 
+let board = [];
+for (let i = 0; i < 6; ++i) {
+    board[i] = [];
+    for (let j = 0; j < 7; ++j) {
+        board[i][j] = (i + 1) * 10 + j + 1;
+    }
+}
 
 generateButtons();
 
@@ -20,7 +19,7 @@ function generateButtons() {
             button.innerText = button.id;
             document.body.appendChild(button);    
             button.onclick = "this.disabled=true";
-            button.onclick = function() {modifyCell(button.id, currentPlayer, i, j); this.disabled=true;}
+            button.onclick = function() {modifyCell(button.id, i, j); this.disabled=true;}
             if (j == 7 ) {
                 document.body.appendChild(document.createElement("br"));
             }
@@ -28,34 +27,33 @@ function generateButtons() {
     } 
 }
 
-function modifyCell(idBtn, currentGamer, lineIndex, colIndex) {
+function modifyCell(idBtn, lineIndex, colIndex) {
     let currentBtn = document.getElementById(idBtn);
     if (weHaveWinner == 0) {
-        if (currentGamer === "red") {
+        if (currentPlayer === "red") {
             currentPlayer = "yellow";
-        } else if (currentGamer === "yellow") {
+        } else if (currentPlayer === "yellow") {
             currentPlayer = "red";
         }
-        currentBtn.style.backgroundColor = currentGamer;
-        mat[lineIndex - 1][colIndex - 1] = currentGamer;
+        currentBtn.style.backgroundColor = currentPlayer;
+        board[lineIndex - 1][colIndex - 1] = currentPlayer;
         document.getElementById("Initial-text").innerHTML = "NO WINNER";
-        checkWinner(weHaveWinner, currentGamer);
+        checkWinner(weHaveWinner);
     }
 }
 
-function checkWinner(weHaveWinner1, currentColor) {
+function checkWinner(weHaveWinner1) {
     weHaveWinner1 = 0;  
     for (let i = 0; i < 6 && weHaveWinner1 == 0; ++i) {
         for (let j = 0; j < 7 && weHaveWinner1 == 0; ++j) {
-            if ((j < 4 &&  mat[i][j] == mat[i][j + 1] && mat[i][j + 1] == mat[i][j + 2] && mat[i][j + 2] == mat[i][j + 3]) || 
-                (i < 4 && mat[i][j] == mat[i + 1][j] && mat[i + 1][j] == mat[i + 2][j] && mat[i + 2][j] == mat[i + 3][j]) ||
-                (i < 4 && j < 4 && mat[i][j] == mat[i + 1][j + 1] && mat[i + 1][j + 1] == mat[i + 2][j + 2] && mat[i + 2][j + 2] == mat[i + 3][j + 3]) ||
-                 i < 4 && j > 3 && (mat[i][j] == mat[i + 1][j - 1] && mat[i + 1][j - 1] == mat[i + 2][j - 2] && mat[i + 2][j - 2] == mat[i + 3][j - 3])) {
-                document.getElementById("Initial-text").innerHTML = "Winner is color: " + currentColor;
+            if ((j < 4 &&  board[i][j] == board[i][j + 1] && board[i][j + 1] == board[i][j + 2] && board[i][j + 2] == board[i][j + 3]) || 
+                (i < 4 && board[i][j] == board[i + 1][j] && board[i + 1][j] == board[i + 2][j] && board[i + 2][j] == board[i + 3][j]) ||
+                (i < 4 && j < 4 && board[i][j] == board[i + 1][j + 1] && board[i + 1][j + 1] == board[i + 2][j + 2] && board[i + 2][j + 2] == board[i + 3][j + 3]) ||
+                 i < 4 && j > 3 && (board[i][j] == board[i + 1][j - 1] && board[i + 1][j - 1] == board[i + 2][j - 2] && board[i + 2][j - 2] == board[i + 3][j - 3])) {
+                document.getElementById("Initial-text").innerHTML = "Winner is color: " + currentPlayer;
                 weHaveWinner1 = 1; 
                 weHaveWinner = weHaveWinner1;
             }    
         }
     }
-  
 }
